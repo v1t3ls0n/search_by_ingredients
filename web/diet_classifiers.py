af629f27cef8a73acec22abd481762182bd8117e
Uri Goren <ugoren@argmaxml.com>
2025-06-01 11:44:40 -0400

    main

--- web/diet_classifiers.py
+++ web/diet_classifiers.py
@@ -1,4 +1,4 @@
-import json
+import json, sys
 from argparse import ArgumentParser
 from typing import List
 from time import time
@@ -10,9 +10,11 @@ except ImportError:
         print("sklearn is not installed, skipping classification report")
 
 def is_ingredient_keto(ingredient: str) -> bool:
+    #TODO: Implement
     return False
 
 def is_ingredient_vegan(ingredient: str) -> bool:
+    #TODO: Implement
     return False
 
 def is_keto(ingredients: List[str]) -> bool:
@@ -24,21 +26,27 @@ def is_vegan(ingredients: List[str]) -> bool:
 def main(args):
     with open(args.ground_truth, "r") as f:
         ground_truth = json.load(f)
-    start_time = time()
-    keto_pred, vegan_pred, keto_gt, vegan_gt = [], [], [], []
-    for ingredient,v in ground_truth.items():
-        keto_pred.append(is_ingredient_keto(ingredient))
-        vegan_pred.append(is_ingredient_vegan(ingredient))
-        keto_gt.append(v['keto'])
-        vegan_gt.append(v['vegan'])
-    end_time = time()
+    try:
+        start_time = time()
+        keto_pred, vegan_pred, keto_gt, vegan_gt = [], [], [], []
+        for ingredient,v in ground_truth.items():
+            keto_pred.append(is_ingredient_keto(ingredient))
+            vegan_pred.append(is_ingredient_vegan(ingredient))
+            keto_gt.append(v['keto'])
+            vegan_gt.append(v['vegan'])
+        end_time = time()
+    except Exception as e:
+        print(f"Error: {e}")
+        return -1
+
     print("===Keto===")
     print(classification_report(keto_gt, keto_pred))
     print("===Vegan===")
     print(classification_report(vegan_gt, vegan_pred))
     print(f"== Time taken: {end_time - start_time} seconds ==")
+    return 0
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--ground_truth", type=str, default="web/data/ingredients.json")
-    main(parser.parse_args())
+    sys.exit(main(parser.parse_args()))
