63792335ba930b064ae9e041c2897f31e317559b
v1t3ls0n <guyvitelson@gmail.com>
2025-06-29 19:55:59 +0300

    fixed import error

--- web/src/diet_classification/evaluation/__init__.py
+++ web/src/diet_classification/evaluation/__init__.py
@@ -5,9 +5,25 @@ Model evaluation and metrics modules.
 """
 
 from .metrics import pack, table
+from .export import (
+    export_results_to_csv,
+    log_false_predictions,
+    save_model_artifacts,
+    aggregate_results_across_domains,
+    export_ensemble_metrics,
+    export_eval_plots
+)
 
 __all__ = [
     # Metrics
     'pack',
-    'table'
-    ]
+    'table',
+    
+    # Export functions
+    'export_results_to_csv',
+    'log_false_predictions',
+    'save_model_artifacts',
+    'aggregate_results_across_domains',
+    'export_ensemble_metrics',
+    'export_eval_plots',
+]
