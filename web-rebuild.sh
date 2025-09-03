d27eefc3510997dbe8758d65cc0a7b8d64e8fde8
v1t3ls0n <guyvitelson@gmail.com>
2025-09-03 22:34:55 +0300

    yessssss

--- web-rebuild.sh
+++
@@ -1,8 +0,0 @@
-docker compose build --no-cache web
-docker compose up -d
-docker compose logs -f web
-# remove the marker and restart web to re-run indexing from init.sh
-docker compose exec web rm -f /app/.indexed
-docker compose restart web
-# tail logs again
-docker compose logs -f web
